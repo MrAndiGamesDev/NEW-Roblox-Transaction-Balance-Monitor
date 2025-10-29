05e9020f065773de9297fddd97aeb57c5508ea26
MrAndi Scripted <andielmendez4@gmail.com>
2025-10-29 13:23:24 -0400

    Updated

--- install.bat
+++ install.bat
@@ -1,7 +1,11 @@
 @ECHO OFF
 set "URL=https://raw.githubusercontent.com/MrAndiGamesDev/NEW-Roblox-Transaction-Balance-Monitor/main/requirements.txt"
 set "OUT=requirements.txt"
-powershell -NoProfile -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%OUT%'"
-echo Download complete: %OUT%
-pip install -r "%OUT%"
+if exist "%OUT%" (
+    echo Found existing %OUT%, skipping download.
+) else (
+    powershell -NoProfile -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%OUT%'"
+    echo Download complete: %OUT%
+)
+pip install -r %OUT%
 pause
