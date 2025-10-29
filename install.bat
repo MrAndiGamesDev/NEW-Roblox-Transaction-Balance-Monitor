d84a5b3bb5205b6bfc4da96a22d7100d621fd3a2
MrAndi Scripted <andielmendez4@gmail.com>
2025-10-29 13:14:36 -0400

    Updated 3 packages

--- install.bat
+++ install.bat
@@ -1,1 +1,9 @@
-pip install -r requirements.txt -y
+@echo off
+setlocal
+set "url=https://raw.githubusercontent.com/MrAndiGamesDev/NEW-Roblox-Transaction-Balance-Monitor/main/requirements.txt"
+set "out=requirements.txt"
+powershell -NoProfile -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%out%'"
+echo Download complete: %out%
+pip install -r %out%
+pause
+endlocal
